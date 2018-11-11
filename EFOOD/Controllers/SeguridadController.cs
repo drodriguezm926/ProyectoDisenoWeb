1f156cd3c9bed019b41ab05e94b4edc6e87d8441
Daniel Leonardo Rodriguez Miranda <drodriguezm926@ulacit.ed.cr>
2018-11-11 08:44:54 -0600

    Upload Project

---
+++ EFOOD/Controllers/SeguridadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFOOD.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Usuario()
+        {
+            ViewBag.Title2 = "Si se puedo";
+            return View();
+        }
+
+        [HttpGet] 
+        public ActionResult AsignarRol()
+        {
+            
+            return View();
+        }
+
+        [HttpGet] 
+        public ActionResult CambiarContrasena()
+        {
+
+            return View();
+        }
+    }
+}
