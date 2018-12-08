e8e0764a73244181f52f1b4629d632e80e09f0b0
Mynor Hernández <mhernandezm004@ulacit.ed.cr>
2018-12-08 12:41:30 -0600

    Kabooooom

---
+++ EFood-ECommerce/Controllers/BusquedaProductoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFood_ECommerce.Controllers
+{
+    public class BusquedaProductoController : Controller
+    {
+        // GET: BusquedaProducto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BusquedaProductos()
+        {
+
+            return View();
+        }
+
+        public ActionResult MostrarProductos()
+        {
+
+            return View();
+        }
+    }
+} 
---
+++ EFood-ECommerce/Models/ProductoModel.cs
@@ -0,0 +1,42 @@
+﻿using EFOOD.Models;
+using EFood_ECommerce.App_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFood_ECommerce.Models
+{
+    public class ProductoModel
+    {
+
+        public string ProductCode { get; set; }
+        public string ProductDescription { get; set; }
+        public string FoodOptionCode { get; set; }
+        public string ProductContent { get; set; }
+        public string ProductImage { get; set; }
+        public HttpPostedFileBase Archivo { get; set; }
+
+        public static List<ProductoModel> ObtenerProductos()
+        {
+            try
+            {
+                using (Entities db = new Entities())
+                {
+                    return (from productos in db.Products
+                            select new ProductoModel
+                            {
+                                ProductCode = productos.ProductCode,
+                                ProductDescription = productos.ProductDescription,
+                                FoodOptionCode = productos.FoodOptionCode,
+                                ProductContent = productos.ProductContent,
+                                ProductImage = productos.ProductImage
+                            }).ToList();
+                }
+
+            }
+            catch (Exception x) { ErrorLogModel.AddError(x); return null; }
+        }
+
+    }
+}
