3f49d723a80be1551253e0aaf060b8ad33b68f6b
Santiago Méndez <imendezc976@ulacit.ed.cr>
2018-12-14 15:21:50 -0600

    Pagina de ver carrito

---
+++ EFood-ECommerce/Controllers/CarritoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFood_ECommerce.Controllers
+{
+    public class CarritoController : Controller
+    {
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult VerCarrito()
+        {
+            return View();
+        }
+    }
+}
