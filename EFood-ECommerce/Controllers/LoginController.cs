983d9eeffdf4c0526b26df561be7803a9d6ef042
Santiago Méndez <imendezc976@ulacit.ed.cr>
2018-12-14 13:50:41 -0600

    Login ECOMMMERS
    Controlador login con paginas login y registro

---
+++ EFood-ECommerce/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFood_ECommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+    }
+}
